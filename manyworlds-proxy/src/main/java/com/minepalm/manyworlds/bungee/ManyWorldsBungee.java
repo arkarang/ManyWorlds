48f1cfe7542b9ba2b2f69133bce0292bc4c8e0da
mrminsu <82914006+arkarang@users.noreply.github.com>
2021-07-10 20:06:17 +0900

    Update ManyWorldsBungee.java

--- manyworlds-proxy/src/main/java/com/minepalm/manyworlds/bungee/ManyWorldsBungee.java
+++ manyworlds-proxy/src/main/java/com/minepalm/manyworlds/bungee/ManyWorldsBungee.java
@@ -3,9 +3,13 @@ package com.minepalm.manyworlds.bungee;
 import com.minepalm.hellobungee.bungee.HelloBungee;
 import com.minepalm.manyworlds.api.*;
 import com.minepalm.manyworlds.api.bukkit.WorldInfo;
+import com.minepalm.manyworlds.bungee.events.WorldLoadEvent;
+import com.minepalm.manyworlds.bungee.events.WorldUnloadEvent;
 import com.minepalm.manyworlds.core.AbstractManyWorlds;
 import com.minepalm.manyworlds.core.database.global.MySQLGlobalDatabase;
 import com.minepalm.manyworlds.core.netty.PacketFactory;
+import com.minepalm.manyworlds.core.netty.PacketResolver;
+import com.minepalm.manyworlds.core.netty.WorldLoadPacket;
 import lombok.Getter;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Plugin;
@@ -32,9 +36,19 @@ public class ManyWorldsBungee extends Plugin implements BungeeView {
         inst = this;
         conf = new Conf(this, "config.yml", true);
         serverName = conf.getName();
-        database = new MySQLGlobalDatabase(serverName, this, conf.getServerTable(), conf.getWorldsTable(), conf.getProperties());
+        database = new MySQLGlobalDatabase(serverName, this, conf.getServerTable(), conf.getWorldsTable(), conf.getProperties(), Executors.newScheduledThreadPool(4));
         core = new ProxyCore(this, database, new BukkitWorldController(HelloBungee.getConnections()));
+
+        Listener packetListener = new Listener(new PacketResolver(Executors.newSingleThreadExecutor(), database));
+        packetListener.register(WorldLoadPacket.class, packet->{
+            if(packet.isLoad()){
+                ProxyServer.getInstance().getPluginManager().callEvent(new WorldLoadEvent(packet.getSampleName(), packet.getWorldName()));
+            }else
+                ProxyServer.getInstance().getPluginManager().callEvent(new WorldUnloadEvent(packet.getSampleName(), packet.getWorldName()));
+        });
+
         ProxyServer.getInstance().getPluginManager().registerCommand(this, new Commands());
+        ProxyServer.getInstance().getPluginManager().registerListener(this, packetListener);
     }
 
     @Override
