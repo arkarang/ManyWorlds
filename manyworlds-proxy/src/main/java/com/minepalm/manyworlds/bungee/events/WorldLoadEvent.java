e223a6864dced669190317481fdeae09d514c22a
mrminsu <82914006+arkarang@users.noreply.github.com>
2021-07-10 20:05:19 +0900

    added bunge world event

---
+++ manyworlds-proxy/src/main/java/com/minepalm/manyworlds/bungee/events/WorldLoadEvent.java
@@ -0,0 +1,11 @@
+package com.minepalm.manyworlds.bungee.events;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import net.md_5.bungee.api.plugin.Event;
+
+@Getter
+@RequiredArgsConstructor
+public class WorldLoadEvent extends Event {
+    final String sampleName, worldName;
+}
