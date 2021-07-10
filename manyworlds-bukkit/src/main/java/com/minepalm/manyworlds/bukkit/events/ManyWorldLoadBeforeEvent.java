f27df1e558ed5802e68fe46ab0164cb71f91c334
mrminsu <82914006+arkarang@users.noreply.github.com>
2021-07-10 20:00:47 +0900

    implements events

---
+++ manyworlds-bukkit/src/main/java/com/minepalm/manyworlds/bukkit/events/ManyWorldLoadBeforeEvent.java
@@ -0,0 +1,31 @@
+package com.minepalm.manyworlds.bukkit.events;
+
+import com.minepalm.manyworlds.api.bukkit.ManyWorld;
+import com.minepalm.manyworlds.api.bukkit.WorldInfo;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class ManyWorldLoadBeforeEvent extends Event implements Cancellable {
+    private static final HandlerList handlers = new HandlerList();
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+    @Getter
+    @Setter
+    public boolean cancelled;
+
+    final WorldInfo worldInfo;
+    final ManyWorld manyWorld;
+}
